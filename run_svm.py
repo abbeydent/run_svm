6228a1adfa9a1419d5c2f211c745bb4c42920dbd
Abbey Dent <adent@g.clemson.edu>
2019-05-21 15:33:40 -0400

    rearrange

--- run_svm.py
+++ run_svm.py
@@ -5,18 +5,6 @@ from sklearn.datasets.samples_generator import make_blobs
 from sklearn.svm import SVC
 
 
-X, Y = make_blobs(n_samples=70, centers=2, random_state=0, cluster_std=0.7)
-
-
-# print(X)
-# print(Y)
-
-
-plt.scatter(X[:,0], X[:,1], c = Y)
-plt.savefig('scatterplot.png')
-
-svm_model = SVC(kernel = 'linear', C = 1E10)
-svm_model.fit(X,Y)
 
 def plot_decision_function(model):
 	ax = plt.gca()
@@ -37,8 +25,23 @@ def plot_decision_function(model):
 	ax.set_xlim(xlim)
 	ax.set_ylim(ylim)
 
+
+X, Y = make_blobs(n_samples=70, centers=2, random_state=0, cluster_std=0.7)
+
+
+# print(X)
+# print(Y)
+
+
+plt.scatter(X[:,0], X[:,1], c = Y)
+plt.savefig('scatterplot.png')
+
+svm_model = SVC(kernel = 'linear', C = 1E10)
+svm_model.fit(X,Y)
+
 plot_decision_function(svm_model)
 plt.savefig('scatterplot_with_decision_function.png')
+
 
 
 
