ca57fff24a7f9ea9aea816b7006cb97d4760b568
Abbey Dent <adent@g.clemson.edu>
2019-05-21 15:08:07 -0400

    create scatterplot

--- run_svm.py
+++ run_svm.py
@@ -6,5 +6,9 @@ from sklearn.datasets.samples_generator import make_blobs
 X, Y = make_blobs(n_samples=70, centers=2, random_state=0, cluster_std=0.7)
 
 
-print(X)
-print(Y)
+# print(X)
+# print(Y)
+
+
+plt.scatter(X[:,0], X[:,1], c = Y)
+plt.savefig('scatterplot.png')
