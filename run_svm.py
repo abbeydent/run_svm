665ab726be980616363149f551b34fe4123baa86
Abbey Dent <adent@g.clemson.edu>
2019-05-21 15:26:11 -0400

    construct support vector classifier model

--- run_svm.py
+++ run_svm.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt 
 from sklearn.datasets.samples_generator import make_blobs
 
+from sklearn.svm import SVC
+
 
 X, Y = make_blobs(n_samples=70, centers=2, random_state=0, cluster_std=0.7)
 
@@ -12,3 +14,13 @@ X, Y = make_blobs(n_samples=70, centers=2, random_state=0, cluster_std=0.7)
 
 plt.scatter(X[:,0], X[:,1], c = Y)
 plt.savefig('scatterplot.png')
+
+svm_model = SVC(kernel = 'linear', C = 1E10)
+svm_model.fit(X,Y)
+
+
+
+
+
+
+
